975c4c36593c59792fe7cab51083b4bee7aa4936
oussaka <oussaka@users.noreply.github.com>
2018-05-06 11:05:50 +0000

    ramdom access Voter

---
+++ src/Security/Voter/RandomAccessVoter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Security\Voter;
+
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class RandomAccessVoter extends Voter
+{
+    protected function supports($attribute, $subject)
+    {
+        return $attribute === 'RANDOM_ACCESS';
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token): bool
+    {
+        return random_int(0, 10) > 5;
+    }
+}
